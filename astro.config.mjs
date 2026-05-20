0d87dc20913fc549dfd845de7ca704f3ec736b84
Skippies <leroy.curtis.barnes@gmail.com>
2026-05-20 20:20:23 +0200

    fix: remove GitHub project base path for custom domain (#53)
    Co-authored-by: L.U.M.O.S. <lumos@lbdc.co.za>

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,5 +3,5 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://skippies-io.github.io',
-  base: '/lbdc-website',});
+  site: 'https://www.lbdc.co.za',
+});
